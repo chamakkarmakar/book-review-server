034c743b761c814db26096f308b688396c8e5d4e
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-04 18:45:40 +0600

    get data from db and post data to db

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const port = process.env.PORT || 5000;
 const app = express();
+// const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.co1ibvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.co1ibvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pm0abs7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
- 
-    await client.connect();
-    
-    console.log("Database is connected");
-  } finally {
-   
-    await client.close();
-  }
+    try {
+        await client.connect();
+        const bookDB = client.db("bookDB");
+        const reviewsCollection = bookDB.collection("reviewsCollection");
+
+        // get all data 
+        app.get("/books", async (req, res) => {
+            const booksData = reviewsCollection.find();
+            const result = await booksData.toArray();
+            res.send(result);
+        });
+
+        // create data 
+        app.post("/books", async (req, res) => {
+            const booksData = req.body;
+            console.log(booksData);
+            const result = await reviewsCollection.insertOne(booksData);
+            console.log(result);
+            res.send(result);
+            
+        });
+
+        console.log("Database is connected");
+    } finally {
+
+        await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Route is working");
-  });
-  
-  app.listen(port, (req, res) => {
+});
+
+app.listen(port, (req, res) => {
     console.log("App is listening on port :", port);
-  });
+});
 
 
 
