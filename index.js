35e5e7e5b64fdb777fca1bad61933dc9850ca395
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-05 20:42:52 +0600

    update

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Route is working");
+    res.send("Book Route is working");
 });
 
 app.listen(port, (req, res) => {
