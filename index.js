f21228082ba4e8dd28fde791016ae325cd85e43d
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-18 01:34:35 +0600

    jwt

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+// const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        
+    ],
+    credentials: true
+}));
+
 app.use(express.json());
+// app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pm0abs7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,7 +35,7 @@ function createToken(user) {
             email: user.email,
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "7d" }
+        { expiresIn: "30d" }
     );
     return token;
 }
@@ -107,8 +116,9 @@ async function run() {
                 });
             }
             await userCollection.insertOne(user);
-            return res.send({ token });;
+            return res.send({ token });
         });
+        
 
         app.get("/user", async (req, res) => {
             const usersData = userCollection.find();
